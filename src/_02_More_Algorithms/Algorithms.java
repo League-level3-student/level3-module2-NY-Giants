57f4d06b1b164a45c5ae6b7784b7eecabf9be917
League Student <league@iMac-8.attlocal.net>
2018-12-30 17:25:33 -0800

    MoreandMoreAlgorithims(IP)
    gg

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -3,9 +3,29 @@ package _02_More_Algorithms;
 import java.util.List;
 
 public class Algorithms {
-	public static int findBrokenEgg(List<String> eggs) {
-		for(int i = 0; i < 0; i++) {
-			
+	public static Object findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+	public static Object countPearls(List<Boolean> oysters) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i).equals(true)) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) >= 6.6) {
+				return i;
+			}
 		}
 		return 0;
 	}
