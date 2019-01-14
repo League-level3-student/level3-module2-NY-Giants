14a0a1a7881e03f9916a54aac8f280bbf148a644
League Student <league@iMac-75.attlocal.net>
2019-01-13 17:29:09 -0800

    Algorithims(IP), MoreAndMoreAlgorithims(IP)
    gg

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -2,6 +2,8 @@ package _02_More_Algorithms;
 
 import java.util.List;
 
+import _00_Sorting_Algorithms.SortingVisualizer;
+
 public class Algorithms {
 	public static Object findBrokenEgg(List<String> eggs) {
 		for (int i = 0; i < eggs.size(); i++) {
@@ -22,11 +24,48 @@ public class Algorithms {
 	}
 
 	public static double findTallest(List<Double> peeps) {
+		Double ali_a = peeps.get(0);
 		for (int i = 0; i < peeps.size(); i++) {
-			if (peeps.get(i) >= 6.6) {
-				return i;
+			if (peeps.get(i) >= ali_a) {
+				ali_a = peeps.get(i);
 			}
 		}
-		return 0;
+		return ali_a;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String ali_a3 = words.get(0);
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > ali_a3.length()) {
+				ali_a3 = words.get(i);
+			}
+		}
+		return ali_a3;
+	}
+
+	public static boolean containsSOS(List<String> message1) {
+		for (int i = 0; i < message1.size(); i++) {
+			if (message1.get(i).trim().equals("... --- ...")) {
+				return true;
+			}
+		
+		}
+		return false;
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+			for(int i = 0; i < results.size(); i++) {
+				for(int j = 0; j < results.size() - i - 1; j++) {
+					if(results.get(j) < results.get(j+1)) {
+						
+					}
+					else {
+						Double temp = results.get(j);
+						results.set(j, results.get(j+1));
+						results.set(j+1, temp);
+					}
+				}
+			}
+		return results;
 	}
 }
