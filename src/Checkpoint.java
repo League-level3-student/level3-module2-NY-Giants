953d5d3c50dbc85a2695ef9498c3f2e117fde1be
NY-Giants <league@iMac-2.attlocal.net>
2019-01-27 17:27:28 -0800

    Checkpoint(IP)
    gg

---
+++ src/Checkpoint.java
@@ -0,0 +1,36 @@
+
+public class Checkpoint {
+	public static void main(String[] args) {
+		System.out.println("(Microphone Static) Testing, attention please");
+	}
+	void doubletake(Double[] morethanonce){
+		for(int i = 0; i < morethanonce.length; i++) {
+			for(int j = 0; j < morethanonce.length - i - 1; j++) {
+				if(morethanonce[j] < morethanonce[j+1]) {
+					
+				}
+				else {
+					double temp = morethanonce[j];
+					morethanonce[j] = morethanonce[j+1];
+					morethanonce[j+1] = temp;
+				}
+		}
+	}
+}
+	boolean integertake(int[] inttakeem, int g) {
+		for(int i = 0; i < inttakeem.length; i++) {
+			for(int j = 0; j < inttakeem.length - i - 1; j++) {
+				if(inttakeem[j] < inttakeem[j+1]) {
+					
+				}
+				else {
+					int temp = inttakeem[j];
+					inttakeem[j] = inttakeem[j+1];
+					inttakeem[j+1] = temp;
+				}	
+		}
+		
+	}
+		return false;
+	}
+}
