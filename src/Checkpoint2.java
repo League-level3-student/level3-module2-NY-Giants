567cecdca0ba42719ede3e0e0665843020ce155a
NY-Giants <league@iMac-2.attlocal.net>
2019-02-17 17:01:50 -0800

    Checkpoint2
    gg

--- src/Checkpoint2.java
+++ src/Checkpoint2.java
@@ -1,6 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class Checkpoint2 {
-public static void main (String[] args) {
-	int[ ] ALT = new int[5];
-}
+	public static void main(String[] args) {
+		int[] ALT = new int[5];
+		String ET;
+		int DD;
+		for (int i = 0; i < ALT.length; i++) {
+			ET = JOptionPane.showInputDialog("Pick a number");
+			DD = Integer.parseInt(ET);
+		}
+		for (int j = 0; j < ALT.length; j++) {
+			JOptionPane.showMessageDialog(null, ALT);
+		}
+	}
 }
